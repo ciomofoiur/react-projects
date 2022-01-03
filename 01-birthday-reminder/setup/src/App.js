d3c4b3048069540215966f4ecd662895d483c679
Raoul Ciomofoiu <raoul.ciomofoiu@itperspectives.ro>
2022-01-03 12:53:12 +0200

    Completed

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List';
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
 function App() {
   const [people, setPeople] = useState(data);
 
@@ -12,7 +12,7 @@ function App() {
 
   return (
     <main>
-      <secttion className='container'>
+      <secttion className="container">
         <h3>{people?.length} birthdays today</h3>
         <List people={people}>
           <span>Happy birthday!</span>
@@ -24,3 +24,5 @@ function App() {
 }
 
 export default App;
+
+//completed
